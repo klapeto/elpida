871cd4638acb17d51c2227f7d5522aba4458ffdf
Ioannis Panagiotopoulos <yiannispana@hotmail.com>
2019-11-25 00:51:26 +0200

    Fixed Memory Read volatile

--- TaskBatches/Memory/Read/Volatile/MemoryReadVolatile.hpp
+++ TaskBatches/Memory/Read/Volatile/MemoryReadVolatile.hpp
@@ -40,7 +40,52 @@ namespace Elpida
 
 			virtual void run() override
 			{
-
+				volatile auto ptr = (T*) this->_memory.getPointer();
+				register auto start = ptr;
+				register auto end = (T *) ((T) start + this->_memory.getSize());
+				register auto iterations = this->_iterations;
+				register auto x = T();
+				for (register auto i = 0ul; i < iterations; ++i)
+				{
+					ptr = start;
+					while (ptr < end)
+					{
+						x = *ptr;
+						x = *(ptr + 1);
+						x = *(ptr + 2);
+						x = *(ptr + 3);
+						x = *(ptr + 4);
+						x = *(ptr + 5);
+						x = *(ptr + 6);
+						x = *(ptr + 7);
+						x = *(ptr + 8);
+						x = *(ptr + 9);
+						x = *(ptr + 10);
+						x = *(ptr + 11);
+						x = *(ptr + 12);
+						x = *(ptr + 13);
+						x = *(ptr + 14);
+						x = *(ptr + 15);
+						x = *(ptr + 16);
+						x = *(ptr + 17);
+						x = *(ptr + 18);
+						x = *(ptr + 19);
+						x = *(ptr + 20);
+						x = *(ptr + 21);
+						x = *(ptr + 22);
+						x = *(ptr + 23);
+						x = *(ptr + 24);
+						x = *(ptr + 25);
+						x = *(ptr + 26);
+						x = *(ptr + 27);
+						x = *(ptr + 28);
+						x = *(ptr + 29);
+						x = *(ptr + 30);
+						x = *(ptr + 31);
+						ptr += 32;
+					}
+				}
+				auto dummy = x;
 			}
 
 			MemoryReadVolatile(const Memory& memory)
