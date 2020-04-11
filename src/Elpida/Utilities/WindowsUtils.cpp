a2d4c8fcfcc08ac7dbe50458e3b6a18b43559172
klapeto <yiannispana@hotmail.com>
2020-04-12 01:22:50 +0300

    Windows Build fix

--- src/Elpida/Utilities/WindowsUtils.cpp
+++ src/Elpida/Utilities/WindowsUtils.cpp
@@ -14,12 +14,13 @@ namespace Elpida
 #ifdef ELPIDA_WINDOWS
 	std::string WindowsUtils::GetLastErrorString()
 	{
+
+		LPSTR messageBuffer = nullptr;
 		try
 		{
 			DWORD errorMessageID = GetLastError();
 			if (errorMessageID == 0) return std::string();
-
-			LPSTR messageBuffer = nullptr;
+			
 			size_t size = FormatMessageA(
 				FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
 				NULL, errorMessageID, MAKELANGID(LANG_NEUTRAL, SUBLANG_NEUTRAL), (LPSTR) & messageBuffer, 0, NULL);
