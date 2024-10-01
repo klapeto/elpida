acc024424e15facc1f3dca0d1be64b5e9683fe2c
klapeto <yiannispana@hotmail.com>
2024-10-01 11:24:18 +0300

    Fix SSL validation errors on upload

--- apps/Application/DataUploader.cpp
+++ apps/Application/DataUploader.cpp
@@ -1,10 +1,27 @@
 //
+//  Copyright (c) 2024  Ioannis Panagiotopoulos
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+//
 // Created by klapeto on 8/9/2023.
 //
 
 #include "DataUploader.hpp"
 
 #include "ResultSerializer.hpp"
+#include "Elpida/Core/Config.hpp"
 
 #include "httplib.hpp"
 
@@ -25,7 +42,13 @@ namespace Elpida::Application
 
 		httplib::Client client(apiUrl);
 
-		client.set_write_timeout(Seconds(30));
+		client.set_read_timeout(Seconds(60));
+		client.set_write_timeout(Seconds(60));
+		client.set_connection_timeout(Seconds(300));
+#ifdef ELPIDA_UNIX
+		// we need this because the SSL_CTX_set_default_verify_paths() internally points to who knows where
+		client.set_ca_cert_path("", "/etc/ssl/certs/");
+#endif
 		auto httpResponse =
 				client.Post(resultPath,
 						{{ apiKeyHeader, apiKey }},
