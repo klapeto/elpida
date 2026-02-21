3a150090476b53b1e8c86da03b001f5964650a4a
klapeto <yiannispana@hotmail.com>
2026-02-21 18:51:23 +0200

    Fixes for dependencies on linux/update base scores

--- apps/Application/BenchmarkBaseScores.hpp
+++ apps/Application/BenchmarkBaseScores.hpp
@@ -118,9 +118,9 @@ namespace Elpida::Application
                     "AES Decryption",
                     {
                         "9a44b227-39b1-41d6-b8fc-18243d9c9744",
-                        54092929.288530,
+                        54084870.835872,
                         "eb8aa408-5d7f-4d8e-8a37-e57edc011c63",
-                        107703787.465754,
+                        108145416.699462,
                         Generate<AESDecryption>,
                         false
                     }
@@ -129,9 +129,9 @@ namespace Elpida::Application
                     "AES Encryption",
                     {
                         "59248a7c-5d7f-4f1e-b01f-3c3cbe2e7437",
-                        54440260.673872,
+                        54446997.602133,
                         "9e2fb41c-146f-4725-8ca0-1e19503b03c7",
-                        108938249.333609,
+                        108910983.463203,
                         Generate<AESEncryption>,
                         false
                     }
@@ -140,9 +140,9 @@ namespace Elpida::Application
                     "Base64 Decode",
                     {
                         "0a48078b-b924-43a9-97d9-6a9fcb2ebb45",
-                        20772991.291928,
+                        20996485.911489,
                         "060af447-f649-4a5b-b60a-0579616cada0",
-                        41560930.967016,
+                        41997770.013692,
                         Generate<Base64Decode>,
                         false
                     }
@@ -151,9 +151,9 @@ namespace Elpida::Application
                     "Base64 Encode",
                     {
                         "b793d108-7603-4aa7-b2ae-4c2f8eba98a7",
-                        92321758.530595,
+                        91870416.441277,
                         "00a8b5a5-eb3e-45b5-bced-f90895103f7d",
-                        183212745.378985,
+                        183364916.476990,
                         Generate<Base64Encode>,
                         false
                     }
@@ -162,9 +162,9 @@ namespace Elpida::Application
                     "C++ Compilation",
                     {
                         "a60442f2-1bc7-4df6-9d83-18dcda067b51",
-                        0.365401,
+                        0.358185,
                         "b7c330be-90ba-4ac5-a84f-03cd8c77fd85",
-                        0.685072,
+                        0.673625,
                         Generate<CppCompilation>,
                         false
                     }
@@ -173,9 +173,9 @@ namespace Elpida::Application
                     "FFT calculation in place",
                     {
                         "a8946ba5-049f-4626-9b1a-d5d31211cea1",
-                        17800925.881694,
+                        19231980.050714,
                         "554f2321-95df-43c0-af46-90554e26c8cd",
-                        35831095.384151,
+                        38435583.023591,
                         Generate<FFT>,
                         false
                     }
@@ -184,9 +184,9 @@ namespace Elpida::Application
                     "Json Parsing",
                     {
                         "cc53b078-75aa-41c0-8885-b381bc1a4bda",
-                        20248464.671295,
+                        20046152.440443,
                         "35f22cbc-16e7-4c35-aebf-029dac328e46",
-                        40416457.972433,
+                        40160625.414452,
                         Generate<JsonParsing>,
                         false
                     }
@@ -195,9 +195,9 @@ namespace Elpida::Application
                     "Matrix inverse (4x4)",
                     {
                         "7930adf1-fd99-420a-9a39-b4e147f1bdf2",
-                        33158697.548213,
+                        36097438.492493,
                         "3faca5e9-d9ac-47a5-a8d4-c331666c6e6a",
-                        69272015.832837,
+                        69727090.261137,
                         Generate<MatrixInverse>,
                         false
                     }
@@ -206,9 +206,9 @@ namespace Elpida::Application
                     "Matrix multiplication (32x32)",
                     {
                         "56834364-d449-4d3e-bc50-197c697c6c6f",
-                        73369557.735944,
+                        73971453.852095,
                         "a28c475e-4541-4dcb-80d4-14583c325a7c",
-                        133084849.304717,
+                        146387009.246924,
                         Generate<MatrixMultiplication>,
                         false
                     }
@@ -217,9 +217,9 @@ namespace Elpida::Application
                     "NBody Simulation",
                     {
                         "9196f126-391f-452b-9ca0-0dd39d7f70ae",
-                        19150991.914668,
+                        19146921.238315,
                         "6b01f47a-4f41-4f13-96e9-836baf6bed90",
-                        35887061.057429,
+                        36646461.385687,
                         Generate<NBody>,
                         false
                     }
@@ -228,9 +228,9 @@ namespace Elpida::Application
                     "3D Ray Tracing",
                     {
                         "ad8da8ff-a1f3-4a21-9234-43b6aff8eea4",
-                        895832.576202,
+                        899963.260796,
                         "5c888135-bab1-4651-9407-2d7187f54381",
-                        1781805.694223,
+                        1799442.852409,
                         Generate<RayTracing>,
                         false
                     }
@@ -239,9 +239,9 @@ namespace Elpida::Application
                     "Regex (Boost)",
                     {
                         "bd0d8849-b988-40ac-9961-5e3528327da3",
-                        31935984.281190,
+                        31974527.137750,
                         "142291f8-3c83-4f1b-ab02-ad1914e08885",
-                        63933656.371673,
+                        61809284.016832,
                         Generate<Regex>,
                         false
                     }
@@ -250,9 +250,9 @@ namespace Elpida::Application
                     "RSA Decryption",
                     {
                         "4a4e3a1d-df8b-48d8-8a86-b49fafea79fc",
-                        5750.394066,
+                        5764.552589,
                         "bae420c9-3297-456c-bbc2-6b884af15bb7",
-                        11555.777716,
+                        11579.422195,
                         Generate<RSADecryption>,
                         false
                     }
@@ -261,9 +261,9 @@ namespace Elpida::Application
                     "RSA Encryption",
                     {
                         "32e5628d-aa9a-4ddc-84d0-17779647e22b",
-                        199737.285176,
+                        200808.305751,
                         "021876da-05b4-4408-8d9a-7637035927c2",
-                        402218.286970,
+                        402401.401761,
                         Generate<RSAEncryption>,
                         false
                     }
@@ -272,9 +272,9 @@ namespace Elpida::Application
                     "Svg Rasterization",
                     {
                         "e7ab603e-6c05-428b-9851-1e0a3729e022",
-                        169822.554997,
+                        170698.646107,
                         "66087de6-f486-4d24-b58d-f90300458179",
-                        340784.889555,
+                        339764.774031,
                         Generate<SVGRasterization>,
                         false
                     }
@@ -283,9 +283,9 @@ namespace Elpida::Application
                     "XML Parsing",
                     {
                         "70b7048f-face-4976-b68a-d8cd1004ad33",
-                        42710400.615093,
+                        40788370.208740,
                         "2ac01731-f3de-4fb1-adfc-35937d009097",
-                        84368140.229877,
+                        80561988.005329,
                         Generate<XmlParsing>,
                         false
                     }
@@ -294,9 +294,9 @@ namespace Elpida::Application
                     "Zlib compression",
                     {
                         "895ff7fd-257d-411f-a8a6-de547b0f6485",
-                        21185472.177368,
+                        21165265.770548,
                         "1b4532fd-14f0-41af-ab33-5e4e3803f79b",
-                        42661795.345657,
+                        42601487.668794,
                         Generate<ZlibCompression>,
                         false
                     }
@@ -305,9 +305,9 @@ namespace Elpida::Application
                     "Zlib decompression",
                     {
                         "d40517b1-b39f-4b82-8dc4-b440dd11da1b",
-                        41985061.558662,
+                        42167373.037469,
                         "39d94225-34aa-479b-b9cb-b6e104fb17b9",
-                        84203218.506272,
+                        84236897.036274,
                         Generate<ZlibDecompression>,
                         false
                     }
--- scripts/Linux/getDependencies.sh
+++ scripts/Linux/getDependencies.sh
@@ -18,107 +18,117 @@
 
 set -e
 
+function log() {
+    if [ -z "$VERBOSE" ]; then
+        return
+    fi
+    echo "$1" >&2
+}
+
 function getDependencies() {
     if [ "$#" -ne 3 ]; then
-        echo "Usage: $0 <binary_file> <search_dir> <target_triple>"
+        echo "Usage: $0 <binaryFile> <searchDir> <targetTriple>" >&2
         return 1
     fi
 
-    binary="$1"
+    binaryFile="$1"
     searchDir="$2"
     targetTriple="$3"
 
-    if [ ! -f "$binary" ]; then
-        echo "Error: Binary file '$binary' does not exist or is not a regular file."
+    if [ ! -f "$binaryFile" ]; then
+        echo "Error: Binary file '$binaryFile' does not exist or is not a regular file." >&2
         return 1
     fi
 
     if [ ! -d "$searchDir" ]; then
-        echo "Error: Root directory '$searchDir' does not exist."
+        echo "Error: Root directory '$searchDir' does not exist." >&2
         return 1
     fi
 
     targetArch=$(expr "$targetTriple" : '\(.*\)-.*-.*')
     targetSuffix=$(expr "$targetTriple" : '.*\(-.*-.*\)')
 
+    excludedFiles=$(cat "$this_dir/excludelist" | sed 's|#.*||g')
+
     # trim trailing /
     if [ "$searchDir" != "/" ]; then
         searchDir="${searchDir%/}"
     fi
 
-    searchDirectories=("usr" "lib" "lib64")
+    libraryPaths=$("$targetTriple-clang" --sysroot="$searchDir" -print-search-dirs | grep "libraries:")
+    libraryPaths=$(expr "$libraryPaths" : 'libraries: =\(.*\)')
+    libraryPaths=(${libraryPaths//:/ })
 
-    local allLibs;
-    doGetDependencies "$binary" "$searchDir" "$targetTriple" "$allLibs"
+    local additionalLibs="libatomic.so"
+
+    doGetDependencies "$binaryFile" "$additionalLibs"
 }
 
 function doGetDependencies() {
 
-    local binary="$1"
-    local searchDir="$2"
-    local targetTriple="$3"
+    local binaryFile="$1"
+    local additionalLibs="$2"
 
-    local libs;
-    libs=$(objdump -p "$binary" | awk '/NEEDED/ {print $NF}')
+    log "Getting dependencies of $binaryFile"
+
+    local libs
+    libs=$(objdump -p "$binaryFile" | awk '/NEEDED/ {print $NF}')
 
     if [ -z "$libs" ]; then
+        log "  No libraries are linked to $binaryFile"
         return
     fi
 
+    libs="$libs $additionalLibs"
+
     local libFound=0
     for lib in $libs; do
-        libFound=0;
-        for directory in "${searchDirectories[@]}"; do
+        libFound=0
+
+        if [[ " $excludedFiles " =~ .*$lib.* ]]; then
+            log "  $lib is excluded"
+            continue
+        fi
+        log "  Searching for $lib"
+        for directory in "${libraryPaths[@]}"; do
             local found=""
             local allFiles=""
             local lib_path=""
             if [ $libFound == 1 ]; then
                 # if a previous directory copied, then skipp the rest
-                break;
+                log "    $lib was previously copied. Skipping"
+                break
             fi
 
             # || true are for errors due to link loops
-            found=$(find "$searchDir/$directory" \( -type l -o -type f \) -name "$lib" 2>/dev/null) || true
+            found=$(find "$directory" \( -type l -o -type f \) -name "$lib" 2>/dev/null) || true
             if [ -z "$found" ]; then
+                log "    $lib was not found on $directory"
                 continue
             fi
 
-            # search for the arch specific if needed (in case there is multilib installed)
-            for candidate in $found; do
-                case $candidate in
-                *$targetSuffix*)
-                    if [[ "$candidate" != *"$targetArch"* ]]; then
-                        continue;
-                    fi
-                    lib_path="$candidate"
-                    break;
-                  ;;
-                esac
-            done
+            lib_path=${found[0]}
 
             # check whether we found a link and get all files if needed
             allFiles=$(find -L "$searchDir" -samefile "$lib_path" 2>/dev/null) || true
 
             for candidate in $allFiles; do
-                if [[ "$candidate" != *"$directory"* ]]; then
-                    continue;
-                fi
-
-                if [[ " $allLibs " =~ .*\ $candidate\ .*  ]]; then
+                if [[ " $allLibs " =~ .*\ $candidate\ .* ]]; then
+                    log "      $candidate was previously added. Skipping"
                     libFound=1
-                    continue;
+                    continue
                 fi
 
                 allLibs="$allLibs $candidate"
                 echo "$candidate"
-                doGetDependencies "$candidate" "$searchDir" "$targetTriple" "$allLibs"
+                doGetDependencies "$candidate"
                 libFound=1
             done
         done
 
         if [ $libFound -ne 1 ]; then
-            echo "Library: $lib was not found"
-            return 1;
+            log "  Library: $lib was not found"
+            return 1
         fi
     done
 }
